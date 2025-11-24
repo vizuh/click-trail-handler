46a840829468f5a2b039e94e732d4d891afcedcb
Hugo <hugomoreiradecarvalho@gmail.com>
2025-11-24 02:14:24 +0000

    Improve sanitization of attribution inputs

--- includes/integrations/class-clicktrail-woocommerce.php
+++ includes/integrations/class-clicktrail-woocommerce.php
@@ -2,40 +2,46 @@
 
 class ClickTrail_WooCommerce_Integration {
 
-	public function init() {
-		add_action( 'woocommerce_checkout_create_order', array( $this, 'save_order_attribution' ), 10, 2 );
-	}
+    public function init() {
+        add_action( 'woocommerce_checkout_create_order', array( $this, 'save_order_attribution' ), 10, 2 );
+    }
 
-	/**
-	 * Save attribution data to the order.
-	 *
-	 * @param WC_Order $order
-	 * @param array $data
-	 */
-	public function save_order_attribution( $order, $data ) {
-                $attribution = clicktrail_get_attribution();
-		if ( ! $attribution ) {
-			return;
-		}
+    /**
+     * Save attribution data to the order.
+     *
+     * @param WC_Order $order
+     * @param array    $data
+     */
+    public function save_order_attribution( $order, $data ) {
+        $attribution = clicktrail_get_attribution();
+        if ( ! $attribution ) {
+            return;
+        }
 
-		// First Touch
-		if ( isset( $attribution['first_touch'] ) ) {
-			foreach ( $attribution['first_touch'] as $key => $value ) {
-                                $order->update_meta_data( '_ct_ft_' . $key, $value );
-			}
-		}
-
-		// Last Touch
-		if ( isset( $attribution['last_touch'] ) ) {
-			foreach ( $attribution['last_touch'] as $key => $value ) {
-                                $order->update_meta_data( '_ct_lt_' . $key, $value );
-			}
-		}
-
-		// Session Count
-		if ( isset( $attribution['session_count'] ) ) {
-                        $order->update_meta_data( '_ct_session_count', $attribution['session_count'] );
+        // First Touch
+        if ( isset( $attribution['first_touch'] ) ) {
+            foreach ( $attribution['first_touch'] as $key => $value ) {
+                $meta_key = sanitize_key( $key );
+                if ( '' !== $meta_key ) {
+                    $order->update_meta_data( '_ct_ft_' . $meta_key, sanitize_text_field( $value ) );
                 }
-	}
+            }
+        }
+
+        // Last Touch
+        if ( isset( $attribution['last_touch'] ) ) {
+            foreach ( $attribution['last_touch'] as $key => $value ) {
+                $meta_key = sanitize_key( $key );
+                if ( '' !== $meta_key ) {
+                    $order->update_meta_data( '_ct_lt_' . $meta_key, sanitize_text_field( $value ) );
+                }
+            }
+        }
+
+        // Session Count
+        if ( isset( $attribution['session_count'] ) ) {
+            $order->update_meta_data( '_ct_session_count', absint( $attribution['session_count'] ) );
+        }
+    }
 
 }
