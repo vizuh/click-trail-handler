fee298712851d37bd5d98335beb05dcc3dfb9156
Atroci <hugomoreiradecarvalho@gmail.com>
2025-12-11 20:04:22 -0300

    feat: Add WhatsApp click tracking and logging functionality with a new custom post type and AJAX handler.

---
+++ includes/post-types/class-whatsapp-click.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * WhatsApp Click Post Type
+ *
+ * @package ClickTrail
+ */
+
+namespace CLICUTCL\Post_Types;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class WhatsApp_Click
+ */
+class WhatsApp_Click {
+
+	/**
+	 * Register the custom post type.
+	 */
+	public function register() {
+		register_post_type(
+			'clicutcl_wa_click',
+			array(
+				'labels'       => array(
+					'name'          => __( 'WhatsApp Clicks', 'click-trail-handler' ),
+					'singular_name' => __( 'WhatsApp Click', 'click-trail-handler' ),
+				),
+				'public'       => false,
+				'show_ui'      => true,
+				'show_in_menu' => 'clicutcl-settings',
+				'capability_type' => 'post',
+				'capabilities' => array(
+					'create_posts' => 'do_not_allow',
+				),
+				'map_meta_cap' => true,
+				'supports'     => array( 'title' ),
+			)
+		);
+	}
+}
